fd541ab0e3c3a726da6802e373f244b309d12878
juancarango18 <juan.c.arango@geniussports.com>
2026-04-24 19:06:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -264,18 +264,18 @@ if st.session_state.role == 'user':
 # UI: TEAM LEAD (ADMIN) PORTAL
 # ==========================================
 # ==========================================
-if st.session_state.role == "admin":
+#if st.session_state.role == "admin":
         
         # 1. The Logo goes FIRST!
-        st.image("logo2.png", width=200)
+        #st.image("logo2.png", width=200)
         
         # 2. The Main Title goes SECOND! (Delete the duplicate one if you have two!)
-        st.title("🗓️ Sportzcast Scheduler")
+        #st.title("🗓️ Sportzcast Scheduler")
         
         # 3. Your Selectors go THIRD!
-        col_year, col_month = st.columns(2)
-        with col_year:
-            selected_year = st.selectbox("Select Year", [2024, 2025, 2026])
+        #col_year, col_month = st.columns(2)
+        #with col_year:
+            #selected_year = st.selectbox("Select Year", [2024, 2025, 2026])
         
 if st.session_state.role == 'admin':
     
